0860b6e413d333ae60660a26fb7add3d67a46f7a
alisha-barik <alishabarik1122@gmail.com>
2025-07-06 23:07:09 +0530

    redux implementation

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -5,18 +5,49 @@ import {
   DialogDescription,
 } from "@/components/ui/dialog";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
+import { readFileAsDataURL } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+import { toast } from "sonner";
+import axios from "axios";
 
 const CreatePost = ({ open, setOpen }) => {
   const imageRef = useRef();
+  const [file, setFile] = useState("");
+  const [caption, setCaption] = useState("");
+  const [imagePreview, setImagePreview] = useState("");
+  const [loading, setLoading] = useState(false)
+
+  const fileChangeHandler = async (e) => {
+const file = e.target.files?.[0];
+if(file){
+  setFile(file);
+  const dataUrl = await readFileAsDataURL(file);
+  setImagePreview(dataUrl);
+}
+  }
+
   const createPostHandler = async (e) => {
-    e.preventDefault();
+    const formData = new FormData();
+    formData.append("caption", caption);
+    if(file) formData.append("image", file)
     try {
-      // your logic
-    } catch (err) {
-      console.log(err);
+  setLoading(true);
+      const res = await axios.post('http://localhost:8000/api/v1/post/addpost', formData, {
+        headers:{
+          'Content-Type':'multipart/form-data'
+        },
+        withCredentials:true
+      });
+      if(res.data.success){
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      toast.error(error.response.data.message)
+    } finally{
+      setLoading(false)
     }
   };
 
@@ -25,7 +56,7 @@ const CreatePost = ({ open, setOpen }) => {
       <DialogContent>
         {/* 👇 Add DialogTitle for accessibility */}
         <DialogTitle>Create a New Post</DialogTitle>
-        <div onClick={createPostHandler} className="flex gap-3 items-center">
+        <div className="flex gap-3 items-center">
           <Avatar>
             <AvatarImage src="" alt="image" />
             <AvatarFallback>CN</AvatarFallback>
@@ -36,16 +67,34 @@ const CreatePost = ({ open, setOpen }) => {
           </div>
         </div>
         <Textarea
+        value={caption} onChange={(e) => setCaption(e.target.value)}
           className="focus-visible:ring-transparent border-none"
           placeholder="write a caption..."
         />
-        <input ref={imageRef} type="file" className="hidden" />
+        {
+          imagePreview && (
+            <div className="w-full h-64 flex items-center justify-center">
+              <img src={imagePreview} alt="preview_img" className="object-cover h-full w-full rounded-md"/>
+            </div>
+          )
+        }
+        <input ref={imageRef} type="file" className="hidden" onChange={fileChangeHandler}/>
         <Button
           onClick={() => imageRef.current.click()}
           className="w-fit mx-auto bg-[#0095F6] hover:bg-[#258bcf]"
         >
           Select from computer
         </Button>
+        {imagePreview && (
+loading ? (
+  <Button>
+    <Loader2 className="mr-2 h-4 w-4 animate-spin"/>
+    Please Wait
+  </Button>
+) : (
+  <Button type="submit" onClick={createPostHandler} className="w-full">Post</Button>
+)
+        )}
       </DialogContent>
     </Dialog>
   );
